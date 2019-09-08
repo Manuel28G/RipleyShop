217fb7e5e5b806a5ac06240c938c2354b0ba0aea
Manuel Goncalves <man281192@gmail.com>
2019-09-08 16:49:47 -0300

    Adding Models and Constants

---
+++ app/src/main/java/cl/com/ripley/ripleyshop/general/model/Constants.java
@@ -0,0 +1,7 @@
+package cl.com.ripley.ripleyshop.general.model;
+
+public class Constants {
+
+    public static final String EP_MANAGMENT_PRODUCT = "http://13.57.51.72:3000/";
+    public static final String EP_GET_PRODUCT_INFO = "https://simple.ripley.cl/api/v2/";
+}
