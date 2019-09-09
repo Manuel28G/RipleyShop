cf50a88afabbab322f9a52aa95c92896e9d2b9bc
Manuel Goncalves <man281192@gmail.com>
2019-09-09 03:43:03 -0300

    Change Style and title of lateral menu

--- app/src/main/java/cl/com/ripley/ripleyshop/home/view/activity/MainActivity.java
+++ app/src/main/java/cl/com/ripley/ripleyshop/home/view/activity/MainActivity.java
@@ -118,17 +118,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_techn) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_electronic) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_furniture_home) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_woman_fashion) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sport_adventure) {
 
         }
 
