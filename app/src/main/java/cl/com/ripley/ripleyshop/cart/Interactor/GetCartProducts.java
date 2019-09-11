b5181a1333bd5b7c352d378ce711d891c8dfc25b
Manuel Goncalves <man281192@gmail.com>
2019-09-11 01:52:20 -0300

    Creating interactor classes

---
+++ app/src/main/java/cl/com/ripley/ripleyshop/cart/Interactor/GetCartProducts.java
@@ -0,0 +1,19 @@
+package cl.com.ripley.ripleyshop.cart.Interactor;
+
+import cl.com.ripley.ripleyshop.cart.presenter.ManageCart;
+import cl.com.ripley.ripleyshop.general.model.GeneralInteractor;
+
+public class GetCartProducts  implements GeneralInteractor {
+
+    private ManageCart.GetProduct mGetProduct;
+
+    public GetCartProducts(ManageCart.GetProduct getProduct){
+        mGetProduct = getProduct;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
