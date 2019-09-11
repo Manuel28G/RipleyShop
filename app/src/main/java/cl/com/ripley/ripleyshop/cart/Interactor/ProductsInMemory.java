1800fce9c8e6e11012e9d750752e223ae56b8605
Manuel Goncalves <man281192@gmail.com>
2019-09-11 02:45:36 -0300

    manage data in memory

---
+++ app/src/main/java/cl/com/ripley/ripleyshop/cart/Interactor/ProductsInMemory.java
@@ -0,0 +1,40 @@
+package cl.com.ripley.ripleyshop.cart.Interactor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.com.ripley.ripleyshop.general.model.UtilHelper;
+import cl.com.ripley.ripleyshop.home.model.HomeProduct;
+
+public class ProductsInMemory {
+
+    private List<HomeProduct> mHomeProductList;
+    private static ProductsInMemory sProductInMemory;
+
+    private ProductsInMemory(){
+
+    }
+
+    public static ProductsInMemory getInstance(){
+        if(sProductInMemory == null)
+            sProductInMemory = new ProductsInMemory();
+        return sProductInMemory;
+    }
+
+    public String getProductList(){
+        return UtilHelper.parseObjectToJsonString(mHomeProductList);
+    }
+
+    public void removeProduct(HomeProduct productId){
+        mHomeProductList.remove(productId);
+    }
+
+    public void addProduct(HomeProduct productId){
+        mHomeProductList.add(productId);
+    }
+
+    public void payAction(){
+        mHomeProductList = new ArrayList<>();
+    }
+
+}
